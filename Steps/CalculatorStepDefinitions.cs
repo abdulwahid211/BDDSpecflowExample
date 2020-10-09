697eef98947d597297c70a8a7adaacbad1d032e4
ATG-CORP\Mohammed.Wahid <mohammed.wahid@accounttechnologies.com>
2020-10-09 17:35:58 +0100

    first commit

---
+++ Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,46 @@
+﻿using TechTalk.SpecFlow;
+using System;
+using NUnit.Framework;
+
+namespace RunPractise.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            Assert.Equals(1, 4);
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+         Assert.Pass();
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            //TODO: implement act (action) logic
+
+            Assert.Pass();
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            Assert.Fail();
+        }
+    }
+}
